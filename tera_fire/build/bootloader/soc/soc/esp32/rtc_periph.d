ed64a29ef9cc03b72e98151828a6d2cb6ac0f51e
Sam Hoshyar <hoshyar@gmail.com>
2021-05-11 21:37:59 -0700

    removing build

--- tera_fire/build/bootloader/soc/soc/esp32/rtc_periph.d
+++
@@ -1,3 +0,0 @@
-soc/esp32/rtc_periph.o: \
- /home/sam/esp/esp-idf/components/soc/soc/esp32/rtc_periph.c
-/home/sam/esp/esp-idf/components/soc/soc/esp32/rtc_periph.c:
--- tera_fire/build/bt/host/bluedroid/btc/core/btc_sec.d
+++
@@ -1,3 +0,0 @@
-host/bluedroid/btc/core/btc_sec.o: \
- /home/sam/esp/esp-idf/components/bt/host/bluedroid/btc/core/btc_sec.c
-/home/sam/esp/esp-idf/components/bt/host/bluedroid/btc/core/btc_sec.c:
